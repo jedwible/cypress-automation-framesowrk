5f85e03644f3e2c7f302caae869a2952498a7593
JedW <69050289+jedwible@users.noreply.github.com>
2023-03-31 17:06:23 -0400

    Updates

---
+++ cypress/e2e/automation-test-store/contact-us.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe("Test Contact Us form via Automation Test Store", () => {
+  it("Should be able to submit a successful submission via contact us form", () => {
+    cy.visit("https://www.automationteststore.com/");
+    cy.xpath("//a[contains(@href, 'contact')]").click();
+    cy.get('a[href$="contact"]');
+    cy.get("#ContactUsFrm_first_name").type("My name is mudd");
+    cy.get("#ContactUsFrm_email").type("joe_blogs123@gmail.com");
+    cy.get("#ContactUsFrm_enquiry").type(
+      "Do you provide additional discount on bulk orders?"
+    );
+    cy.get('button[title="Submit"]').click();
+    cy.get(".mb40 > :nth-child(3)").should(
+      "have.text",
+      "Your enquiry has been successfully sent to the store owner!"
+    );
+  });
+});
